7febf623a763dc0c8184422842349ed06425d06d
juanolaechea <42986554+juanolaechea@users.noreply.github.com>
2022-09-20 08:18:47 -0300

    routing and result componene html and ccs

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+
+  edad =25;
+  peso =60;
+  altura=170;
+sexo= "Masculino";
+
+  constructor( private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  cambiarAltura(event:any): void {
+   this.altura=event.target.value;
+
+  }
+  masculino(){
+    this.sexo="Masculino";
+  }
+
+  femenino(){
+    this.sexo="Femenino";
+  }
+  calcularBmi(){
+
+
+    this.router.navigate(['/resultado']);
+
   }
 
 }
